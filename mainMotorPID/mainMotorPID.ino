4bd3e4a87aced2f8965acc921b483aa00c6bf2aa
Bobby Ranjan <bbbranjan@gmail..com>
2017-02-27 16:18:04 +0800

    Straight Line Motion PID

--- mainMotorPID/mainMotorPID.ino
+++ mainMotorPID/mainMotorPID.ino
@@ -15,7 +15,7 @@ double difference;
 double Setpoint, Input, Output;
 //double Kp=1.7, Ki=0, Kd=0;
 //562.25 square wave = one revolution
-PID myPID(&leftEncoderValue, &Output, &rightEncoderValue, 0.5, 0.0, 0.0, DIRECT);
+PID myPID(&leftEncoderValue, &Output, &rightEncoderValue, 0.5, 0, 0, DIRECT);
 //PID(&input, &output, &setpoint, Kp, Ki, Kd, Direction)
 //Parameters: Input - the variable we are trying to control
 //          : Output - the variable that willl be adjusted by PID
@@ -38,7 +38,7 @@ void loop() {
   // put your main code here, to run repeatedly:
     md.setSpeeds(300+Output,300-Output);
     
-   myPID.Compute();
+    myPID.Compute();
     
     //difference = counterA - counterB;
     //difference = abs(difference);
@@ -50,7 +50,15 @@ void loop() {
     Serial.print(", Diff:");
     Serial.println(Output);
 
+    if((leftEncoderValue > 10050.00) || (rightEncoderValue > 10050.00)) {
+      md.setBrakes(400,400);
+      shutdown();
+    }
+
     //Serial.println(difference);
+
+//    delay(50);
+    
 }
 
 void leftEncoderInc(void){
@@ -62,4 +70,10 @@ void rightEncoderInc(void){
   rightEncoderValue++;
   }
 
+void shutdown()
+{
+ // optionally do stop motors dim the LED's etc. 
+ // Serial.print("stopped");  // or other warning 
+ while(1);
+}
   
