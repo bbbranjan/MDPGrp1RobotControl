0bcd3f4057dae298d8756fd53a2b8f0b167dc6fc
dcedjiayi <dcedjiayi@live.com>
2017-03-02 10:24:58 +0800

    Ver 1.0
    Included:
    
    moveForward()
    moveBackward()
    turnLeft()
    turnRight()
    
    *moveBackward() needs more work, but later*

--- IntergratedSketch/IntergratedSketch.ino
+++ IntergratedSketch/IntergratedSketch.ino
@@ -15,7 +15,7 @@ DualVNH5019MotorShield md;
 double leftEncoderValue=0;
 double rightEncoderValue=0;
 double difference;
-double Setpoint, Input, Output;
+double Output = 0;
 double turnLeftEncoderTarget, turnRightEncoderTarget,
        startLeftEncoderValue, startRightEncoderValue;
 
@@ -28,8 +28,31 @@ Parameters: input - the variable we are trying to control
             setpoint - the value we want the input to maintain
             direction - either DIREDCT or REVERSE
 */       
+
+/* 
+  Ultrasonic Connecttion:
+  Pin 1 VCC (URM v3.2) -> VCC (Arduino)
+  Pin 2 GND (URM V3.2) -> GND (Arduino)
+  Pin 4 PWM (URM V3.2) -. Pin 6 (Arduino)
+  Pin 6 COMP/TRIG (URM V3.2) -> Pin 11 (Arduino)
+  
+*/
+int URPWM = 6; // PWM Output 0-25000US, Eevry 50US represent 1cm
+int URTRIG = 11; // //PWM trigger pin
+
+unsigned int Distance = 0;
+uint8_t EnPwmCmd[4] = {0x44, 0x02, 0xbb, 0x01}; // distance measure command
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  md.init(); //initialise the motor drivers
+  pinMode(LEFT_ENCODER, INPUT); //set digital pin 3 as input
+  pinMode(RIGHT_ENCODER, INPUT); // set digital pin 5 as input
+  attachPCINT(digitalPinToPCINT(LEFT_ENCODER), leftEncoderInc, HIGH);
+  attachPCINT(digitalPinToPCINT(RIGHT_ENCODER), rightEncoderInc, HIGH);
+  myPID.SetOutputLimits(-50, 50);
+  myPID.SetMode(AUTOMATIC);
   
 }
 
@@ -37,3 +60,47 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void moveForward(){
+  leftEncoderValue = 0, rightEncoderValue = 0;
+  Output = 0;
+
+  while(leftEncoderValue != 4498.00){
+    md.setSpeed(200+Output, 200-Output);
+  }
+  md.setBrakes(400,400);
+}
+
+void moveBackward(){
+  leftEncoderValue = 0, rightEncoderValue = 0;
+  Output = 0;
+
+  while(leftEncoderValue != 4498.00){
+    md.setSpeeds(200+Output, 200-Output);
+  }
+  md.setBrakes(400,400);
+}
+
+void turnLeft(){
+  leftEncoderValue = 0, rightEncoderValue = 0;
+  Output = 0;
+
+  while((leftEncoderValue - turnLeftEncoderValue <= 800.00) && 
+        (rightEncoderValue - turnRightEncoderValue <= 800.00)){
+          md.setSpeeds(-(200+Output), 200-Output));
+        }
+        md.setBrakes(400,400);
+}
+
+void turnRight(){
+  leftEncoderValue = 0, rightEncoderValue = 0;
+  Output = 0;
+
+  while((leftEncoderValue - turnLeftEncoderValue <= 800.00) && 
+        (rightEncoderValue - turnRightEncoderValue <= 800.00)){
+          md.setSpeeds(200+Output, -(200-Output));
+        }
+        md.setBrakes(400,400);
+  
+}
+
