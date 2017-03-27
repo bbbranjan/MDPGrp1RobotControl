d42a6e14abcf6e0f73b9ba2bca67ba02e0f15791
Bobby Ranjan <bbbranjan@gmail..com>
2017-03-27 11:30:12 +0800

    Resolving Merge Conflicts

--- IntergratedSketch/IntergratedSketch.ino
+++ IntergratedSketch/IntergratedSketch.ino
@@ -258,7 +258,7 @@ void moveForward(double dist) {
   while((leftEncoderValue <= fwd_L_encoder + fwd_dist)|| (rightEncoderValue <= fwd_R_encoder + fwd_dist)) {
     md.setSpeeds(350-Output,350+Output);
     //md.setSpeeds(200+Output,242-Output);
-    md.setSpeeds(200+Output,242-Output);
+//    md.setSpeeds(200+Output,242-Output);
 //    md.setSpeeds(202+Output,233-Output);
     myPID.Compute();
 //    Serial.print("Left:");
